9c24ec5ff2f4a64c010a9fef442b169af589a65a
Alexey Bondarovitch <alexey.bondarovitch@itechart-group.com>
2018-07-24 17:29:11 +0300

    added access and refresh token methods to server

--- src/server/app.js
+++ src/server/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import request from 'request';
 import settings from './constants/settings';
 
 const app = express();
@@ -10,13 +11,79 @@ app.get('/api/login', (req, res) => {
   res.redirect('https://accounts.spotify.com/authorize?' +
     `client_id=${settings.clientId}&response_type=token&` +
     `redirect_uri=${encodeURIComponent(settings.redirectUrl)}`);
+
+app.get('/api/token', function(req, res) {
+  const code = req.query.code || null;
+  const redirect_uri = req.query.redirect_uri || null;
+
+  const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri: redirect_uri,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(settings.client_id + ':' + settings.client_secret).toString('base64'))
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+
+      const access_token = body.access_token;
+      const refresh_token = body.refresh_token;
+
+      res.set({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+      });
+
+      res.json({
+        access_token,
+        refresh_token
+      });
+    } else {
+      res.status(400).json({
+        error: 'error'
+      })
+    }
+  });
 });
 
-app.get('/refresh/:token', (req, res) => {
-  const refreshToken = req.params.token;
-  const authToken = refreshToken;
+app.get('/api/token/refresh', function(req, res) {
 
-  res.send(authToken);
+  // requesting access token from refresh token
+  const refresh_token = req.query.refresh_token;
+  const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: { 'Authorization': 'Basic ' + (new Buffer(settings.client_id + ':' + settings.client_secret).toString('base64')) },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      const access_token = body.access_token;
+
+      res.set({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+      });
+
+      res.json({
+        'access_token': access_token
+      });
+    } else {
+      res.status(400).json({
+        error: 'error'
+      })
+    }
+  });
 });
 
 app.get('*', (req, res) => {
